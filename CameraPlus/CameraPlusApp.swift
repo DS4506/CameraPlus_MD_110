35f4275652bd026e3ef4beeb38ca637d13b32ade
DS4506 <davidsmith4506@gmail.com>
2025-10-04 14:12:59 -0500

    Initial Commit

---
+++ CameraPlus/CameraPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPlusApp.swift
+//  CameraPlus
+//
+//  Created by Willie Earl on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraPlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CameraPlus
+//
+//  Created by Willie Earl on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
